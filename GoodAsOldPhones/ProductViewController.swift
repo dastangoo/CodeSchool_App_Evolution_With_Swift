b22387d048c96cea17938bd23ffb2467da11985a
dastangoo <hamed.dastangoo@gmail.com>
2018-06-28 10:38:19 +0430

    Outlets and Actions. Storyboard Outlets

---
+++ GoodAsOldPhones/ProductViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProductViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by superuser on 6/28/18.
+//  Copyright © 2018 superuser. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = UIImage(named: "download")
+    }
+
+}
