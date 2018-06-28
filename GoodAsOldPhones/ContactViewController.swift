e9a1c6f7d0883d0db0d1959e8785d495946af6a9
dastangoo <hamed.dastangoo@gmail.com>
2018-06-28 11:36:30 +0430

    Scroll Views

---
+++ GoodAsOldPhones/ContactViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ContactViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by superuser on 6/28/18.
+//  Copyright © 2018 superuser. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(scrollView)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSize(width: 365, height: 800)
+    }
+}
